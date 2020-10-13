f0dc7358f51cb1914f0ec815e27de00373a3ccf3
Ingrid <ingrita.rpm@gmail.com>
2020-10-12 23:21:21 -0600

    Agregar y obtener
    Se crearon los metodos agreagar y obtener

--- app.py
+++ app.py
@@ -8,5 +8,19 @@ personas = []
 def inicio():
     return "Bienvenido"
 
+@app.route('/agregar',methods=['POST'])
+def agregarPersona():
+    cuerpo = request.get_json()
+    nombre = cuerpo['nombre']
+    edad = cuerpo['edad']
+    persona = {'nombre':nombre,'edad':edad}
+    global personas
+    personas.append(persona)
+    return jsonify({"mensaje":"Agregado correctamente"})
+
+@app.route('/obtener',methods=['GET'])
+def obtenerPersonas():
+    return jsonify(personas)
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=4000)
